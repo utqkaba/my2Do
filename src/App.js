0831e519d7e3046b13b8049f7155afe7cc3c6f63
utqkaba <utqkaba@gmail.com>
2022-12-05 17:06:11 +0300

    bug fix

--- src/App.js
+++ src/App.js
@@ -1,47 +1,48 @@
-import React, { useState, useEffect } from 'react'
-import './App.css'
-import Form from './components/form'
-import ToDoTasks from './components/todoTasks'
-import DayMonthYear from './components/dayMonthYear'
-import { FaGithub } from 'react-icons/fa'
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Form from "./components/form";
+import ToDoTasks from "./components/todoTasks";
+import DayMonthYear from "./components/dayMonthYear";
+import { FaGithub } from "react-icons/fa";
 
 export default function App() {
-  const [tasks, setTasks] = useState(() => JSON.parse(localStorage.getItem("tasks")) || []);
-
+  const [tasks, setTasks] = useState(
+    () => JSON.parse(localStorage.getItem("tasks")) || []
+  );
+  console.log(tasks);
   useEffect(() => {
-    localStorage.setItem('tasks', JSON.stringify(tasks));
+    localStorage.setItem("tasks", JSON.stringify(tasks));
   }, [tasks]);
 
-  const [formInput, setFormInput] = useState('');
+  const [formInput, setFormInput] = useState("");
 
   const handleChange = (e) => {
-    setFormInput(e.target.value)
-  }
+    setFormInput(e.target.value);
+  };
 
   const handleSubmit = (e) => {
-    e.preventDefault()
-    if (formInput !== '') {
-      const date = new Date().toLocaleDateString()
+    e.preventDefault();
+    if (formInput !== "") {
+      const date = new Date().toLocaleDateString();
       const newTask = {
         date: date,
         task: formInput,
-        completed: false
-      }
-      setTasks([...tasks, newTask])
-      setFormInput('')
+        completed: false,
+      };
+      setTasks([...tasks, newTask]);
+      setFormInput("");
     }
-  }
+  };
 
   const handleComplete = (index) => {
-    const newTasks = [...tasks]
+    const newTasks = [...tasks];
     if (newTasks[index].completed === false) {
-      newTasks[index].completed = true
-
+      newTasks[index].completed = true;
     } else {
-      newTasks[index].completed = false
+      newTasks[index].completed = false;
     }
-    setTasks(newTasks)
-  }
+    setTasks(newTasks);
+  };
 
   // const handleUpdate = (index) => {
   //   const newTasks = [...tasks]
@@ -54,25 +55,42 @@ export default function App() {
   // }
 
   const handleRemove = (index) => {
-    const newTasks = [...tasks]
-    newTasks.splice(index, 1)
-    setTasks(newTasks)
-  }
+    const newTasks = [...tasks];
+    newTasks.splice(index, 1);
+    setTasks(newTasks);
+  };
 
   const handleRemoveAll = () => {
-    setTasks([])
-  }
+    setTasks([]);
+  };
 
   return (
     <div>
-      <header className='header'>
-        <a className='goGit' href="https://github.com/utqkaba" target="_blank" rel="noreferrer"> <FaGithub /> </a>
+      <header className="header">
+        <a
+          className="goGit"
+          href="https://github.com/utqkaba"
+          target="_blank"
+          rel="noreferrer"
+        >
+          {" "}
+          <FaGithub />{" "}
+        </a>
       </header>
       <DayMonthYear />
       <div className="toDoApp">
-        <Form formInput={formInput} handleChange={handleChange} handleSubmit={handleSubmit} />
-        <ToDoTasks tasks={tasks} handleComplete={handleComplete} handleRemove={handleRemove} handleRemoveAll={handleRemoveAll} />
+        <Form
+          formInput={formInput}
+          handleChange={handleChange}
+          handleSubmit={handleSubmit}
+        />
+        <ToDoTasks
+          tasks={tasks}
+          handleComplete={handleComplete}
+          handleRemove={handleRemove}
+          handleRemoveAll={handleRemoveAll}
+        />
       </div>
     </div>
-  )
+  );
 }
--- src/components/todoTasks.js
+++ src/components/todoTasks.js
@@ -1,35 +1,63 @@
 // import React, { useState } from 'react'
-import React from 'react'
+import React from "react";
 import { FaEraser } from "react-icons/fa";
 
 export default function todoTasks(props) {
+  // const [complete, setComplete] = useState(false);
 
-   // const [complete, setComplete] = useState(false);
+  const { tasks, handleComplete, handleRemove, handleRemoveAll } = props;
 
-   const { tasks, handleComplete, handleRemove, handleRemoveAll } = props
-
-   return (
-      <ul className='todo'>
-         {tasks.map((task, index) =>
-            <li key={index}>
-               <div className='checkAndTask'>
-                  <label className='checkContainer'>
-                     {/* <input type="checkbox" onClick={() => [handleComplete(index), setComplete(!complete)]} /> */}
-                     <input type="checkbox" onClick={() => handleComplete(index)} />
-                     <span className='checkmark'> </span>
-                  </label>
-                  <span className={tasks[index].completed ? 'false' : 'true'} >{task.task}</span>
-               </div>
-               {/* <button className='trash' onClick={() => handleUpdate(index)}> <FaEraser style={{ fontSize: '16px' }} /></button> */}
-               <button className='trash' onClick={() => handleRemove(index)}> <FaEraser style={{ fontSize: '16px' }} /></button>
-               {/* <button className='trash' onClick={tasks[index].completed ? () => handleRemove(index) : <span class="popuptext" id="myPopup">Popup text...</span>}> <FaEraser style={{ fontSize: '16px' }} /></button> */}
-            </li>
-         )}
-         {/* {tasks.length > 1 && <p> <button className='deleteAll' onClick={() => handleRemoveAll()}> <i><FaEraser /></i> Delete All </button></p>} */}
-         {tasks.length > 0 && <hr className='endLine' />}
-         {/* {tasks.length > 1 && <button class="button-49" onClick={() => handleRemoveAll()}> <i><FaEraser style={{ fontSize: '16px' }} /></i> Just Delete Completed Task</button>} */}
-         {tasks.length > 1 && <button class="button-49" onClick={() => handleRemoveAll()}> Delete All <i><FaEraser style={{ fontSize: '16px' }} /></i></button>}
-
-      </ul>
-   )
+  return (
+    <ul className="todo">
+      {tasks.map((task, index) => (
+        <li key={index}>
+          <div className="checkAndTask">
+            <label className="checkContainer">
+              {/* <input type="checkbox" onClick={() => [handleComplete(index), setComplete(!complete)]} /> */}
+              {tasks[index].completed ? (
+                <input
+                  type="checkbox"
+                  onClick={() => handleComplete(index)}
+                  checked
+                />
+              ) : (
+                <input
+                  type="checkbox"
+                  onClick={() => handleComplete(index)}
+                  unchecked
+                />
+              )}
+              {/* <input type="checkbox" onClick={() => handleComplete(index)} /> */}
+              <span className="checkmark"> </span>
+            </label>
+            <span
+              className={
+                tasks[index].completed ? "completedTask" : "uncompletedTask"
+              }
+            >
+              {task.task}
+            </span>
+          </div>
+          {/* <button className='trash' onClick={() => handleUpdate(index)}> <FaEraser style={{ fontSize: '16px' }} /></button> */}
+          <button className="trash" onClick={() => handleRemove(index)}>
+            {" "}
+            <FaEraser style={{ fontSize: "16px" }} />
+          </button>
+          {/* <button className='trash' onClick={tasks[index].completed ? () => handleRemove(index) : <span class="popuptext" id="myPopup">Popup text...</span>}> <FaEraser style={{ fontSize: '16px' }} /></button> */}
+        </li>
+      ))}
+      {/* {tasks.length > 1 && <p> <button className='deleteAll' onClick={() => handleRemoveAll()}> <i><FaEraser /></i> Delete All </button></p>} */}
+      {tasks.length > 0 && <hr className="endLine" />}
+      {/* {tasks.length > 1 && <button class="button-49" onClick={() => handleRemoveAll()}> <i><FaEraser style={{ fontSize: '16px' }} /></i> Just Delete Completed Task</button>} */}
+      {tasks.length > 1 && (
+        <button class="button-49" onClick={() => handleRemoveAll()}>
+          {" "}
+          Delete All{" "}
+          <i>
+            <FaEraser style={{ fontSize: "16px" }} />
+          </i>
+        </button>
+      )}
+    </ul>
+  );
 }
