d6143526b43cb4068ca5895ef93d6ed63e20ccf6
utqkaba <utqkaba@gmail.com>
2022-12-05 15:33:52 +0300

    font change

--- src/components/dayMonthYear.js
+++ src/components/dayMonthYear.js
@@ -1,12 +1,11 @@
-import React from 'react'
-import moment from 'moment/moment'
+import React from "react";
+import moment from "moment/moment";
 
 export default function dayMonthYear() {
-
-   return (
-      <div className='dayHeading'>
-         <p className='dayMonthYear'>{moment().format('L')} </p>
-         <p className='dayMonthYear'>{moment().format('dddd')} </p>
-      </div>
-   )
+  return (
+    <div className="dayHeading">
+      <p className="dayMonthYear">{moment().format("L")} </p>
+      <p className="dayMonthYear">{moment().format("dddd")} </p>
+    </div>
+  );
 }
